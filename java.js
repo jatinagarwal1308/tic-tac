feebfebbc50e1b0ac47468e40d920dc50b510128
Jatin Agarwal <aggarwaljatin16@gmail.com>
2019-06-24 23:34:31 +0530

    no message

--- java.js
+++ java.js
@@ -1,88 +1,105 @@
-var lc = 1, fg = 3, p1, p2, elem = document.getElementById("display");
+var lc = 1, fg = 3, p1 = "", p2 = "", elem = document.getElementById("display");
 var a = [["","",""],["","",""],["","",""]] , string = "" ;
 
 function register() 
 {
       p1 = document.getElementById("p1").value ; 
       p2 = document.getElementById("p2").value ; 
+
+      elem.innerHTML = p1+ "'s turn" ;
 	console.log(p1,p2)
 }
 
-function val(i,j)
-{     
-	elem.innerHTML = "p1 turn" ;
+function restart()
+{
+    document.location.reload();
+}
 
-     if( lc%2 == 0)
-     {
-     	a[i][j] = 1 ;
-        string = p1+ "'s turn" ;
-     }
-     else
-     {
-        a[i][j] = 0 ;	
-        string = p2+ "'s turn" ;
-     }
-     
+function val(a,b)
+{
+    if(p1 == "" || p2 == "")
+    {
+        alert("Enter your names first");
+    }
+
+    else
+    {
+        value(a,b);
+    }
+}
+
+function value(i,j)
+{    
+   
+    if(a[i][j] == "")
+    { 
+        if( lc%2 == 0)
+        {
+          a[i][j] = "X" ;
+          string = p1+ "'s turn" ;
+        }
+
+        else
+        {
+          a[i][j] = "O" ;	
+          string = p2+ "'s turn" ;
+        }
+
      document.getElementById(""+i+j).innerHTML = a[i][j] ;
      lc++ ;
 
-     console.log(a[i][j],i,j,p1,p2); 
-    
-     if(a[1][2]==a[2][2])
-     	{console.log("ok")}
+    }
 
-
-
-
-
-
-
-
-
-
-
- // if( lc>5 )
- // {
- //     for (var j = 0, i = 0; j < 2; j++) 
- //     {
- //     	if((a[i][j] == a[i+1][j] == a[i+2][j] == 0)||(a[i][j] == a[i+1][j] == a[i+2][j] == 1))
- //     	{
- //     		fg = a[i][j] + 1 ;
- //     		break ;
- //     	}
+ if( lc>5 )
+ {    
+     for (var j = 0, i = 0; j <= 2; j++) 
+     {
+     	if((a[i][j] == a[i+1][j]) && (a[i+1][j] == a[i+2][j]) && (a[i][j] != ""))    //column
+     	{
+     		fg = a[i][j] ;
+     		break ;
+     	}
      
- //     	else if((a[j][i] == a[j][i+1] == a[j][i+2] == 0)||(a[j][i] == a[j][i+1] == a[j][i+2] == 1))
- //     	{
- //     		fg = a[i][j] + 1 ;
- //     		break ;
- //     	}
- //     }
+     	else if((a[j][i] == a[j][i+1]) && (a[j][i+1] == a[j][i+2]) && (a[j][i] != ""))    //row
+     	{
+     		fg = a[i][j] ;
+     		break ;
+     	}
+     }
  
- //     if(fg == 3)
- //     { 	
- //    	j = 0 ; 
+     if(fg == 3)
+     { 	
+    	j = 0 ; 
 
- //        if((a[i][i] == a[i+1][i+1] == a[i+2][i+2] == 0)||(a[i][i] == a[i+1][i+1] == a[i+2][i+2] == 1))
- //        {
- //     	   fg = a[i][j] + 1 ;
- //        }
+        if((a[i][i] == a[i+1][i+1]) && (a[i+1][i+1] == a[i+2][i+2]) && (a[i][i] != ""))   //diagonal
+        {
+     	   fg = a[i][j] ;
+        }
 
- //        else if((a[2][0] == a[1][1] == a[0][2] == 0)||(a[2][0] == a[1][1] == a[0][2] == 1))
- //        {
- //        	fg = a[i][j] + 1 ;
- //        }
- //     }
- // }
+        else if((a[2][0] == a[1][1]) && (a[1][1] == a[0][2]) && (a[1][1] != ""))     //diagonal
+        {
+        	fg = a[i][j] ;
+        }
+     }
+ }
 
- // if( fg == 3 && lc == 10)
- // {
- // 	string = "TIE" ; 
- // }
+ if( fg == 3 && lc == 10)
+ {
+ 	string = "TIE" ; 
+ }
 
- // else if(fg == 1 || fg == 2)
- // {
- // 	string = "player"+fg+"wins" ; 
- // }
+ else if(fg == "O" || fg == "X")
+ {
+    if(fg == "O")
+ 	{
+        string = p1 + "wins" ;
+    }
+
+    else
+    {
+        string = p2 + "wins" ;
+    }
+ }
 
   elem.innerHTML = string ;
 
